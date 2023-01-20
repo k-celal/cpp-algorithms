9f050a17c1d0bea81145c8a35e114dd99db0da62
Celal <celal51@gmail.com>
2023-01-21 00:39:01 +0300

    update

--- convertSortedArray/src/main.cpp
+++ convertSortedArray/src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include "vector"
 
-int main(int argc, char *argv[])
+struct TreeNode
 {
-	std::cout << "Hello world!" << std::endl;
-}
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+	vector<int> v;
+
+public:
+	TreeNode *util(int left, int right)
+	{
+		if(left>right){
+			return nullptr;
+		}
+		int mid=(left+right)/2;
+		TreeNode* root = new TreeNode*(v[mid])
+		root->left=util(left,mid-1);
+		root->right=util(mid+1,right);
+		return root;
+	}
+	TreeNode *sortedArrayToBST(vector<int> &nums)
+	{
+		v = nums;
+		return util(0, nums.size() - 1, )
+	}
+};
